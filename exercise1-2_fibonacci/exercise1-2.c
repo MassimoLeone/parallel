92f04164d19ad7cdece66450a5c18e69d4cccee1
MaximArts <maxim.lewtschenko@gmail.com>
2016-04-25 01:21:48 -0700

    correction

--- exercise1-2_fibonacci/exercise1-2.c
+++ exercise1-2_fibonacci/exercise1-2.c
@@ -99,13 +99,9 @@ long long fibonacci_iterative (int n)
 
 long long fibonacci_recursive (int n)
 {
-  if (n == 1)
+  if (n <= 1)
   {
-    return 0;
-  }
-  if (n == 2)
-  {
-    return 1;
+    return n;
   }
 
   return fibonacci_recursive (n - 1) + fibonacci_recursive (n - 2);
--- exercise1-3_sorting/exercise1-3.c
+++ exercise1-3_sorting/exercise1-3.c
@@ -34,66 +34,66 @@ void selectionSort(int*, int);
 
 int main (void)
 {
-  Clock bubble_FL,
-        bubble_SL,
-        selection_FL,
-        selection_SL;
-  int rand_FL_1 [LIMIT_50K];
-  int rand_FL_2 [LIMIT_50K];
-  int rand_SL_1 [LIMIT_100K];
-  int rand_SL_2 [LIMIT_100K];
+  Clock bubble_50K,
+        bubble_100K,
+        selection_50K,
+        selection_100K;
+  int rand_50K_1 [LIMIT_50K];
+  int rand_50K_2 [LIMIT_50K];
+  int rand_100K_1 [LIMIT_100K];
+  int rand_100K_2 [LIMIT_100K];
 
   srand (time (NULL));
 
-  initArrayRandom(rand_FL_1, LIMIT_50K);
-  copyArray(rand_FL_2, rand_FL_1, LIMIT_50K);
-  initArrayRandom(rand_SL_1, LIMIT_100K);
-  copyArray(rand_SL_2, rand_SL_1, LIMIT_100K);
-
-  /* 50K LIMIT */
-  bubble_FL.start_CPU = clock ();
-  bubble_FL.start_Wall = time (NULL);
-  bubbleSort (rand_FL_1, LIMIT_50K);
-  bubble_FL.end_CPU = clock ();
-  bubble_FL.end_Wall = time (NULL);
-
-  bubble_FL.total_CPU = bubble_FL.end_CPU - bubble_FL.start_CPU;
-  bubble_FL.total_Wall = bubble_FL.end_Wall - bubble_FL.start_Wall;
-
-  selection_FL.start_CPU = clock ();
-  selection_FL.start_Wall = time (NULL);
-  selectionSort (rand_FL_2, LIMIT_50K);
-  selection_FL.end_CPU = clock ();
-  selection_FL.end_Wall = time (NULL);
-
-  selection_FL.total_CPU = selection_FL.end_CPU - selection_FL.start_CPU;
-  selection_FL.total_Wall = selection_FL.end_Wall - selection_FL.start_Wall;
-
-  /* 100K LIMIT */
-  bubble_SL.start_CPU = clock ();
-  bubble_SL.start_Wall = time (NULL);
-  bubbleSort (rand_SL_1, LIMIT_100K);
-  bubble_SL.end_CPU = clock ();
-  bubble_SL.end_Wall = time (NULL);
-
-  bubble_SL.total_CPU = bubble_SL.end_CPU - bubble_SL.start_CPU;
-  bubble_SL.total_Wall = bubble_SL.end_Wall - bubble_SL.start_Wall;
-
-  selection_SL.start_CPU = clock ();
-  selection_SL.start_Wall = time (NULL);
-  selectionSort (rand_SL_2, LIMIT_100K);
-  selection_SL.end_CPU = clock ();
-  selection_SL.end_Wall = time (NULL);
-
-  selection_SL.total_CPU = (selection_SL.end_CPU - selection_SL.start_CPU);
-  selection_SL.total_Wall = selection_SL.end_Wall - selection_SL.start_Wall;
+  initArrayRandom(rand_50K_1, LIMIT_50K);
+  copyArray(rand_50K_2, rand_50K_1, LIMIT_50K);
+  initArrayRandom(rand_100K_1, LIMIT_100K);
+  copyArray(rand_100K_2, rand_100K_1, LIMIT_100K);
 
   printf ("Algorithm \t array size \t CPU time \t elapsed time\n");
   printf ("\t \t \t \t (in seconds) \t (in seconds)\n");
-  printf ("Bubblesort \t %i \t\t %.3f \t\t %.3f\n", LIMIT_50K, (float) bubble_FL.total_CPU / CLOCKS_PER_SEC, (float) bubble_FL.total_Wall);
-  printf ("Bubblesort \t %i \t %.3f \t %.3f\n", LIMIT_100K, (float) bubble_SL.total_CPU / CLOCKS_PER_SEC, (float) bubble_SL.total_Wall);
-  printf ("Selection sort \t %i \t\t %.3f \t\t %.3f\n", LIMIT_50K, (float) selection_FL.total_CPU / CLOCKS_PER_SEC, (float) selection_FL.total_Wall);
-  printf ("Selection sort \t %i \t %.3f \t %.3f\n", LIMIT_100K, (float) selection_SL.total_CPU / CLOCKS_PER_SEC, (float) selection_SL.total_Wall);
+
+  /* 50K LIMIT */
+  bubble_50K.start_CPU = clock ();
+  bubble_50K.start_Wall = time (NULL);
+  bubbleSort (rand_50K_1, LIMIT_50K);
+  bubble_50K.end_CPU = clock ();
+  bubble_50K.end_Wall = time (NULL);
+
+  bubble_50K.total_CPU = bubble_50K.end_CPU - bubble_50K.start_CPU;
+  bubble_50K.total_Wall = bubble_50K.end_Wall - bubble_50K.start_Wall;
+  printf ("Bubblesort \t %i \t\t %.3f \t\t %.3f\n", LIMIT_50K, (float) bubble_50K.total_CPU / CLOCKS_PER_SEC, (float) bubble_50K.total_Wall);
+
+  selection_50K.start_CPU = clock ();
+  selection_50K.start_Wall = time (NULL);
+  selectionSort (rand_50K_2, LIMIT_50K);
+  selection_50K.end_CPU = clock ();
+  selection_50K.end_Wall = time (NULL);
+
+  selection_50K.total_CPU = selection_50K.end_CPU - selection_50K.start_CPU;
+  selection_50K.total_Wall = selection_50K.end_Wall - selection_50K.start_Wall;
+  printf ("Selection sort \t %i \t\t %.3f \t\t %.3f\n", LIMIT_50K, (float) selection_50K.total_CPU / CLOCKS_PER_SEC, (float) selection_50K.total_Wall);
+
+  /* 100K LIMIT */
+  bubble_100K.start_CPU = clock ();
+  bubble_100K.start_Wall = time (NULL);
+  bubbleSort (rand_100K_1, LIMIT_100K);
+  bubble_100K.end_CPU = clock ();
+  bubble_100K.end_Wall = time (NULL);
+
+  bubble_100K.total_CPU = bubble_100K.end_CPU - bubble_100K.start_CPU;
+  bubble_100K.total_Wall = bubble_100K.end_Wall - bubble_100K.start_Wall;
+  printf ("Bubblesort \t %i \t %.3f \t %.3f\n", LIMIT_100K, (float) bubble_100K.total_CPU / CLOCKS_PER_SEC, (float) bubble_100K.total_Wall);
+
+  selection_100K.start_CPU = clock ();
+  selection_100K.start_Wall = time (NULL);
+  selectionSort (rand_100K_2, LIMIT_100K);
+  selection_100K.end_CPU = clock ();
+  selection_100K.end_Wall = time (NULL);
+
+  selection_100K.total_CPU = (selection_100K.end_CPU - selection_100K.start_CPU);
+  selection_100K.total_Wall = selection_100K.end_Wall - selection_100K.start_Wall;
+  printf ("Selection sort \t %i \t %.3f \t %.3f\n", LIMIT_100K, (float) selection_100K.total_CPU / CLOCKS_PER_SEC, (float) selection_100K.total_Wall);
 
   return EXIT_SUCCESS;
 }
@@ -133,23 +133,26 @@ void bubbleSort (int* array, int length)
   }
 }
 
-void selectionSort( int *array, int length) // Anzahl und Array mit Daten
+void selectionSort( int *array, int length)
 {
-  int i, k, t, min; // i ist Kontrollvariable für die innere, k für die äußere Schleife.
-                    // t ist Zwischenspeicher beim Tauschen. min merkt sich den kleinsten Wert
-                    // im Teilarray.
+  int outside,
+      inside,
+      tmp,
+      min;
 
-  for( i = 0; i < length-1; i++)
+  for(outside = 0; outside < length-1; outside++)
   {
-    min = i;
-    for( k = i+1; k < length; k++)
+    min = outside;
+    for( inside = outside+1; inside < length; inside++)
     {
-      if( array[k] < array[min])
-        min = k;
+      if(array[inside] < array[min])
+      {
+        min = inside;
+      }
     }
-    t = array[min];  // Tauschen
-    array[min] = array[i];
-    array[i] = t;
+    tmp = array[min];  // exchange
+    array[min] = array[outside];
+    array[outside] = tmp;
   }
 }
 
